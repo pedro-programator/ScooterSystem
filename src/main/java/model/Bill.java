43c46902abc72cf4798376b85ec5f616be935abe
Albert Miller <archalbertmiller@gmail.com>
2019-07-21 15:20:33 +0200

    Created Final Method for Bill

--- src/main/java/model/Bill.java
+++ src/main/java/model/Bill.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 import java.time.chrono.ChronoLocalDateTime;
 import java.time.temporal.ChronoUnit;
 
@@ -15,12 +16,52 @@ public class Bill {
     }
 
 
-    public double givePrice(){
-        long minutes = ChronoUnit.MINUTES.between(rental.getStart(),rental.getEnd());
+    public double givePriceOrdinary() {
+        long minutes = ChronoUnit.MINUTES.between(rental.getStart(), rental.getEnd());
         double price = minutes * ordinaryTarif;
         return price;
     }
 
+    public double givePriceRushHour() {
+        long minutes = ChronoUnit.MINUTES.between(rental.getStart(), rental.getEnd());
+        double price = minutes * rushHourTarif;
+        return price;
+    }
+
+    public double giveHolidaysPrice() {
+        long minutes = ChronoUnit.MINUTES.between(rental.getStart(), rental.getEnd());
+        double price = minutes * holidaysTarif;
+        return price;
+    }
+
+    public double givePriceForAll() {
+        double price = 0.0;
+        if (rental.getStart().getDayOfWeek() == DayOfWeek.SATURDAY || rental.getStart().getDayOfWeek() == DayOfWeek.SUNDAY) {
+            price = giveHolidaysPrice();
+        } else {
+            price = countPriceForWeekDays();
+        }
+        return price;
+    }
+
+    public double countPrice() {
+        double price = 0.0;
+        LocalDateTime temp;
+        for (int i = 0; i < ChronoUnit.MINUTES.between(rental.getStart(), rental.getEnd()); i++) {
+            temp = rental.getStart().plusMinutes(i);
+
+            if (temp.getDayOfWeek() == DayOfWeek.SATURDAY || temp.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                price += holidaysTarif;
+            }else{
+                if (temp.getHour() == 8 || temp.getHour() == 18) {
+                    price += rushHourTarif;
+                } else {
+                    price += ordinaryTarif;
+            }
+            }
+        }
+        return price;
+    }
 //    public double giveRushHourPrice(){
 //        if (rental.getStart().getDayOfWeek()!= DayOfWeek.SATURDAY && rental.getStart().getDayOfWeek()!= DayOfWeek.SUNDAY){
 //
