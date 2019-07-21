0aea04e0cb0c2a9a444d9ff0098a3aa9e0bc76a8
Albert Miller <archalbertmiller@gmail.com>
2019-07-21 13:58:58 +0200

    Created Working test for givePrice() method in Bill class

--- src/test/java/BillTest.java
+++ src/test/java/BillTest.java
@@ -1,10 +1,13 @@
+import model.Bill;
 import model.Rental;
 import model.Scooter;
-import org.assertj.core.api.Assert;
+import org.assertj.core.api.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BillTest {
 
@@ -12,6 +15,7 @@ public class BillTest {
     LocalDateTime start1 = LocalDateTime.of(2019,7,22,10,15);
     LocalDateTime end1 = LocalDateTime.of(2019,7,22,11,15);
     Rental rental = new Rental(start1,end1,scooter1);
+    Bill bill1 = new Bill(rental);
 
 
 //    @BeforeEach
@@ -23,6 +27,8 @@ public class BillTest {
 //    }
     @Test
     void checkPricingMethod(){
+        double expected = 60.0;
+        assertEquals(expected, bill1.givePrice());
 
     }
 }
