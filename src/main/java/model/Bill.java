dd94105223dec07f0c4adaad301821afe7e027b5
Albert Miller <archalbertmiller@gmail.com>
2019-07-21 12:47:11 +0200

    Add Bill class

--- src/main/java/model/Bill.java
+++ src/main/java/model/Bill.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Bill {
+    private Rental rental;
+    private double rushHourTarif = 2.0;
+    private double ordinaryTarif = 1.0;
+    private double holidaysTarif = 0.5;
+
+    public Bill(Rental rental) {
+        this.rental = rental;
+    }
+
+
 }
