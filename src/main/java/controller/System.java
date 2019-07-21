95f5218a9f970ff83e8d9910edf49335bef3e3ca
pedro-programator <p.p.szczepaniak@gmail.com>
2019-07-21 13:53:54 +0200

    change scooterID field name

--- src/main/java/controller/System.java
+++ src/main/java/controller/System.java
@@ -6,11 +6,8 @@ import model.Scooter;
 import model.User;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 public class System {
-
-
     private DataBase dataBase;
 
     public System() {
@@ -20,7 +17,7 @@ public class System {
     public boolean rentScooter(User user, int IDScooter) {
 
         for (Scooter scooter : dataBase.getScooterList()) {
-            if (scooter.getIDThisScooter() == IDScooter) {
+            if (scooter.getScooterID() == IDScooter) {
                 if (!scooter.isRent()) {
                     Rental rental = new Rental(LocalDateTime.now(), scooter);
                     user.addRental(rental);
